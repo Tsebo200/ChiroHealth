71aabbb4a49307ce8a4ade71ffcf11ac02c1624c
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-06-28 12:28:35 +0200

    Register added inputs

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useState } from 'react';
 import styles from '../Pages/Register.module.scss';
 import { Box, Button, Grid, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Register() {
 
@@ -11,8 +13,17 @@ export default function Register() {
     const name = event.target.name;
     const value = event.target.value;
     setInputs(values => ({...values, [name]: value}));
+  }
 
-    console.log(inputs);
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(inputs);
+
+  axios.post('http://localhost/api/AddPatient.php', inputs)
+  .then(function(res){
+      console.log(res);
+  });
+
 
   }
 
@@ -29,74 +40,80 @@ export default function Register() {
               </div>
               <div className={styles.form_container}>
               <Grid container>
+                <div className={styles.imageArea}>
+                    <p className="upload-text">Please Upload a Profile Image</p>
+                    <input name="imageUrl" className={styles.imgInput} type="file" onChange={imageVal} />
+                    <div id="profileimg" className={styles.profile_img}></div>  
+                    </div>
                   <Grid item xs = {6}>
                       <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px', float: 'left'}}>  
-                          <TextField fullWidth name="name" label="Name" variant="outlined" size="small" helperText="Enter First Name"/>
+                          <TextField fullWidth name="name" type='text' label="Name" variant="outlined" size="small" helperText="Enter First Name" onChange={handleChange}/>
                       </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px', float: 'left'}}>  
-                      <TextField fullWidth name="surname" label="Surname" variant="outlined" size="small" helperText="Enter Surname"/>
+                      <TextField fullWidth name="surname" type='text' label="Surname" variant="outlined" size="small" helperText="Enter Surname" onChange={handleChange}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="username" label="Username" variant="outlined" size="small" helperText="Enter Username"/>
+                      <TextField fullWidth name="username" type='text' label="Username" variant="outlined" size="small" helperText="Enter Username" onChange={handleChange}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="email" label="Email" variant="outlined" size="small" helperText="Enter Email "/>
+                      <TextField fullWidth name="email" type='text' label="Email" variant="outlined" size="small" helperText="Enter Email" onChange={handleChange}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="age" label="Age" variant="outlined" size="small" helperText="Enter Age" 
+                      <TextField fullWidth type="number" name="age" label="Age" variant="outlined" size="small" helperText="Enter Age" onChange={handleChange}
                        mask="(#00) 000-0000"definitions={{'#': /[1-9]/,}}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="gender" label="Gender" variant="outlined" size="small" helperText="Enter Gender"/>
+                      <TextField fullWidth type="text" name="gender" label="Gender" variant="outlined" size="small" helperText="Enter Gender" onChange={handleChange}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="phone" label="Phone number" variant="outlined" size="small" helperText="Enter Phone Number"/>
+                      <TextField fullWidth type="number" name="phone" label="Phone number" variant="outlined" size="small" helperText="Enter Phone Number" onChange={handleChange}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="rank" label="Rank" variant="outlined" size="small" helperText="Enter Rank"/>
+                      <TextField fullWidth name="rank" type="text" label="Rank" variant="outlined" size="small" helperText="Enter Rank" onChange={handleChange}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="password" label="Password" variant="outlined" size="small" helperText="Enter Password"/>
+                      <TextField fullWidth name="password" type='password' label="Password" variant="outlined" size="small" helperText="Enter Password" onChange={handleChange}/>
                   </Box>
               </Grid>
 
               <Grid item xs = {6}>
                   <Box sx={{width: 500, maxWidth:'85%', color: 'white', marginLeft: '15px'}}>  
-                      <TextField fullWidth name="confirm password" label="Confirm Password" variant="outlined" size="small" helperText="Confirm Password"/>
+                      <TextField fullWidth type='password' name="confirm password" label="Confirm Password" variant="outlined" size="small" helperText="Confirm Password"/>
                   </Box>
               </Grid>
 
               <Box sx={{width: 500, maxWidth: '85%', marginTop: '15px', marginLeft: '25px'}}>
-              <Button  fullWidth variant="contained">Register</Button>
+              <Button  fullWidth variant="contained" type="submit" onClick={handleSubmit}>Register</Button>
+              <p className={styles.member_text}>Already a member?  <Link to="/login" underline="hover">{'Signin'}</Link></p>
               </Box>
               </Grid>
             </div>
             </div>
-            </div>
+          </div>
             <div className={styles.right}>
                 <div className={styles.logo_box}></div>
             </div>
